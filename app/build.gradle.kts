8c6c605f0ba0cdaff124dd37ce1c3c7b97ae4792
DaManMikey <nexasstudiosdevelopment@gmail.com>
2026-03-31 15:09:48 +0200

    Upgrade SDK versions and version name
    Updated compileSdk and targetSdk versions to 36, and incremented versionName to 1.0.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 
 android {
     namespace = "com.example.quickeditapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.quickeditapp"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
